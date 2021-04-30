1831d4e9ffdf20c03b008fe4c0e27a47030a84af
GooieJS <conradklek@gmail.com>
2021-04-30 17:06:19 -0500

    coco

--- js/main.js
+++ js/main.js
@@ -1,39 +1,8 @@
-let logo = document.getElementById("logo");
-logo.addEventListener("click", ()=>{dark()});
-function dark(){
-  let root = document.getElementById("root");
-  if (root.textContent === ":root{--dark: white;--light: black;}"){
-    root.textContent = ":root{--dark: black;--light: white;}"
-  } else {
-    root.textContent = ":root{--dark: white;--light: black;}"
-  }
-}
 let menu = document.getElementById("menu");
 menu.addEventListener("click", ()=>{side()});
 function side(){
   document.body.classList.toggle("side");
 }
-let carets = document.querySelectorAll(".caret");
-carets.forEach(caret => {
-  caret.addEventListener("click", (e) =>{
-    caret.parentElement.classList.toggle("active");
-  });
-});
-let colors = ["blue", "cyan", "red", "magenta", "purple", "teal", "green", "cool", "gray", "warm"];
-let book = document.getElementById("color");
-for (let color of colors){
-    for (let i = 10; i > 0; i--) {
-        let swatch = document.createElement("div");
-        book.appendChild(swatch);
-        swatch.classList.add("b-" + color + (i * 10));
-    }
-}
-let $colors = document.querySelectorAll("#color div");
-$colors.forEach($color => {
-  $color.addEventListener("click", (e) =>{
-    document.body.classList = "side " + $color.className
-  });
-});
 var proxy = {
   skew: 0
 },
@@ -62,4 +31,24 @@ ScrollTrigger.create({
 gsap.set("section", {
   transformOrigin: "center",
   force3D: true
+});
+
+var isInViewport = function (elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+window.addEventListener("scroll", function(){
+  let images = document.querySelectorAll(".img");
+  images.forEach(image =>{
+    if (isInViewport(image)){
+      image.classList = "img isVisible"
+    } else {
+      image.classList = "img"
+    }
+  })
 });
